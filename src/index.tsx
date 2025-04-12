3d35bd86213456c994c72487d4291c5b2695b372
steffegit <stefanga2020@gmail.com>
2025-04-12 16:35:03 +0300

    feat: added shadcn

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import App from 'App'
 import { BrowserRouter } from 'react-router-dom'
+import './index.css'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
@@ -9,5 +10,5 @@ const root = createRoot(container)
 root.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
 )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <h1>Home Page</h1>;
-};
+  return <h1>Home Page</h1>
+}
 
-export default Home;
+export default Home
